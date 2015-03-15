678b91504f883c95e0287fa533ba5d4663f4d02f
Ole Kristian Pedersen <okpedersen@gmail.com>
2015-03-15 18:45:44 +0100

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+SCRIPT_DIR=$0:A:h;
+
+cd $SCRIPT_DIR;
+
+git pull origin master;
+git submodule update;
+
+function syncFiles () {
+    rsync                     \
+        --exclude ".git/"     \
+        --exclude "setup.sh"  \
+        --exclude "README.md" \
+        -avh --no-perms       \
+        . ~;
+    source ~/.zshrc;
+}
+
+syncFiles;
+unset synFiles;
