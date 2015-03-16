8c6b275e8b9e7f1cd583aced6a28c7a7f2362940
Ole Kristian Pedersen <okpedersen@gmail.com>
2015-03-16 21:32:10 +0100

    setup.sh->update.sh; minor fixes; comments

--- update.sh
+++ update.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env zsh
 
+# This script updates the the repository
+# It doesn't compile submodules (like YCM), which must be done manually
+
 SCRIPT_DIR=$0:A:h;
 
 cd $SCRIPT_DIR;
 
 git pull origin master;
-git submodule update;
+git submodule update --init --recursive;
 
 function syncFiles () {
     rsync                       \
         --exclude ".git/"       \
         --exclude ".gitignore"  \
         --exclude ".gitmodules" \
-        --exclude "setup.sh"    \
+        --exclude "update.sh"   \
         --exclude "README.md"   \
         -avh --no-perms         \
         . ~;
