64b06a974a7672d83da2bf3ebbe547c62f610433
Ole Kristian Pedersen <okpedersen@gmail.com>
2016-02-12 09:55:29 +0100

    Minor changes to update.py

--- update.py
+++ update.py
@@ -2,50 +2,49 @@
 import os
 import sys
 
-# assumes the dotfile directory is located in ~
-# TODO: allow arguments
-DOTFILES_DIR = os.path.dirname(os.path.abspath(__file__))
-HOME_DIR = os.path.dirname(DOTFILES_DIR)
 
-os.chdir(DOTFILES_DIR)
+def main():
+    # assumes the dotfile directory is located in ~
+    # TODO: allow arguments
+    DOTFILES_DIR = os.path.dirname(os.path.abspath(__file__))
+    HOME_DIR = os.path.dirname(DOTFILES_DIR)
 
-# fetch lates changes
-cmd = 'git pull'
-if os.system(cmd):
-    print('{} failed'.format(cmd))
-    sys.exit(1)
-cmd = 'git submodule update --init --recursive'
-if os.system(cmd):
-    print('{} failed'.format(cmd))
-    sys.exit(1)
+    os.chdir(DOTFILES_DIR)
 
-# exclude files only requred for VCS/setup
-EXCLUDE_FILES = set(('.git', '.gitignore', '.gitmodules', 'update.py', 'LICENSE', 'README.md'))
-files = set(os.listdir(DOTFILES_DIR)) - EXCLUDE_FILES
+    commands = ['git pull', 'git submodule update --init --recursive']
+    for cmd in commands:
+        if os.system(cmd):
+            print('{} failed'.format(cmd))
+            sys.exit(1)
 
-# handle collisions
-collisions = set(os.listdir(HOME_DIR)) & files
-# TODO: handle collisions gracefully
-if collisions:
-    print("Got the following collisions:")
-    print("\n".join(collisions))
-    answer = input("Link non-colliding files? (y/[n]) ")
-    if answer.lower().startswith('y'):
-        files -= collisions
-    else:
+    # exclude files only requred for VCS/setup
+    EXCLUDE_FILES = set(('.git', '.gitignore', '.gitmodules',
+                         'update.py', 'LICENSE', 'README.md'))
+    files = set(os.listdir(DOTFILES_DIR)) - EXCLUDE_FILES
+
+    # handle collisions
+    collisions = set(os.listdir(HOME_DIR)) & files
+    # TODO: handle collisions gracefully
+    if collisions:
+        print("Got the following collisions:")
+        print("\n".join(collisions))
+        answer = input("Link non-colliding files? (y/[n]) ")
+        if answer.lower().startswith('y'):
+            files -= collisions
+        else:
+            sys.exit(1)
+
+    print("Linking the following files:")
+    print("\n".join(files))
+    answer = input("Continue? (y/[n]) ")
+    if not answer.lower().startswith('y'):
         sys.exit(1)
 
-print("Linking the following files:")
-print("\n".join(files))
-answer = input("Continue? (y/[n]) ")
-if not answer.lower().startswith('y'):
-    sys.exit(1)
+    # assumes unix (windows requries target_is_directory=True for directories)
+    for f in files:
+        os.symlink(os.path.join(DOTFILES_DIR, f),
+                   os.path.join(HOME_DIR, f))
 
-# assumes unix (windows requries target_is_directory=True for directories)
-for f in files:
-    os.symlink(
-            os.path.join(DOTFILES_DIR, f),
-            os.path.join(HOME_DIR, f),
-    )
+    print("done")
 
-print("done")
+main()
