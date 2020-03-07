3a45c39545dd69a80f24f0413c1d610620da13c6
Ole Kristian Pedersen <okpedersen@gmail.com>
2020-03-07 14:35:57 +0100

    ♻️ Move utilities to separate file

---
+++ lib/util.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+is_macos() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# utility functions
+debug() {
+  echo "[DEBUG]: $1" >&2
+}
+
+info() {
+  echo "[INFO]: $1" >&2
+}
+
+warn() {
+  echo "[WARNING]: $1" >&2
+}
+
+confirm() {
+  local message
+  if [ $# -eq 0 ]; then
+    message="Are you sure?"
+    warn "No message supplied!"
+  else
+    message="$1"
+  fi
+
+  message="$message [y/N] "
+
+  local response
+  read -p "$message" -r response
+  if [[ $response =~ ^[Yy]$ ]]; then
+    return 0; # true
+  fi
+
+  return 1; # false
+}
+
+utility_exists() {
+  if command -v "$1" >/dev/null || \
+     (is_macos && open -Ra "$1" 2>/dev/null); then
+    return 0
+  else
+    return 1
+  fi
+}
+
