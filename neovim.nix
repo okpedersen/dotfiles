b5b4fa75d83c2f3445174d8492203d47ff99cfb2
Ole Kristian Pedersen <okpedersen@gmail.com>
2021-06-20 18:22:21 +0200

    vscode: Move to vscode-neovim extensions

--- neovim.nix
+++ neovim.nix
@@ -94,123 +94,128 @@
       vim-nix
     ];
     extraConfig = ''
-      " General configuration {{{
+      if !exists('g:vscode')
+        " General configuration {{{
 
-      let g:uname = system("uname -a")
-      let g:is_macos = g:uname =~ "Darwin"
-      let g:is_wsl = g:uname =~ "Microsoft"
+        let g:uname = system("uname -a")
+        let g:is_macos = g:uname =~ "Darwin"
+        let g:is_wsl = g:uname =~ "Microsoft"
 
-      set hidden " hide buffers when not displayed
+        set hidden " hide buffers when not displayed
 
-      set nowrap " disable automatic wrapping
+        set nowrap " disable automatic wrapping
 
-      set formatoptions-=ro " no automatic comment leader after <Enter> or 'o'/'O'
+        set formatoptions-=ro " no automatic comment leader after <Enter> or 'o'/'O'
 
-      " show line number of current line, but use relative numbers on other lines
-      set number
-      set relativenumber
+        " show line number of current line, but use relative numbers on other lines
+        set number
+        set relativenumber
 
-      " search options
-      set hlsearch   " highlight
-      set ignorecase " ignore case
-      set smartcase  " ... except when using uppercase letters
-      set incsearch  " find the next match while typing
+        " search options
+        set hlsearch   " highlight
+        set ignorecase " ignore case
+        set smartcase  " ... except when using uppercase letters
+        set incsearch  " find the next match while typing
 
-      " confirm quit, and prompt to save, when exiting unsaved file
-      set confirm
+        " confirm quit, and prompt to save, when exiting unsaved file
+        set confirm
 
-      " predictable splits
-      set splitright
-      set splitbelow
+        " predictable splits
+        set splitright
+        set splitbelow
 
-      " default indent settings
-      set expandtab    " tabs to spaces
-      set tabstop=2    " width of <TAB>
-      set shiftwidth=2 " length of a shift
+        " default indent settings
+        set expandtab    " tabs to spaces
+        set tabstop=2    " width of <TAB>
+        set shiftwidth=2 " length of a shift
 
-      " don't print ins-completion-menu messages
-      set shortmess+=c
+        " don't print ins-completion-menu messages
+        set shortmess+=c
 
-      " avoid jumping when signs appear and disappear
-      set signcolumn=yes
+        " avoid jumping when signs appear and disappear
+        set signcolumn=yes
 
-      " two lines for cmd
-      set cmdheight=2
+        " two lines for cmd
+        set cmdheight=2
 
-      " write swap file after 300ms of inactivity
-      set updatetime=300
+        " write swap file after 300ms of inactivity
+        set updatetime=300
 
-      " python host progs
-      let g:loaded_python_provider = 0 " disable Python 2
-      let g:python3_host_prog="~/.nix-profile/bin/nvim-python3"
-      "}}}
+        " python host progs
+        let g:loaded_python_provider = 0 " disable Python 2
+        let g:python3_host_prog="~/.nix-profile/bin/nvim-python3"
+        "}}}
 
-      " Miscellaneous utilities {{{
+        " Miscellaneous utilities {{{
 
-      " reload vim config after save
-      augroup reload_myvimrc
-        autocmd!
-        autocmd BufWritePost $MYVIMRC source $MYVIMRC
-      augroup END
+        " reload vim config after save
+        augroup reload_myvimrc
+          autocmd!
+          autocmd BufWritePost $MYVIMRC source $MYVIMRC
+        augroup END
 
-      " enable highlight of current line
-      augroup enable_cursorline
-        autocmd!
-        autocmd BufEnter * set cursorline
-        autocmd BufLeave * set nocursorline
-      augroup END
+        " enable highlight of current line
+        augroup enable_cursorline
+          autocmd!
+          autocmd BufEnter * set cursorline
+          autocmd BufLeave * set nocursorline
+        augroup END
 
-      " terminal settings
-      augroup terminal_settings
-        autocmd!
-        autocmd WinEnter,BufEnter term://* startinsert
-        autocmd BufLeave term://* stopinsert
-      augroup END
-      " }}}
+        " terminal settings
+        augroup terminal_settings
+          autocmd!
+          autocmd WinEnter,BufEnter term://* startinsert
+          autocmd BufLeave term://* stopinsert
+        augroup END
+        " }}}
 
-      " Custom keyboard remaps {{{
+        " Custom keyboard remaps {{{
 
-      " use space as map leader
-      let mapleader=" "
-      let maplocalleader="\\"
+        " use space as map leader
+        let mapleader=" "
+        let maplocalleader="\\"
 
-      " use jj as escape button in insert mode
-      inoremap jk <ESC>
+        " use jj as escape button in insert mode
+        inoremap jk <ESC>
 
-      " use <ESC> to escape insertion in terminal
-      tnoremap <ESC> <C-\><C-n>
+        " use <ESC> to escape insertion in terminal
+        tnoremap <ESC> <C-\><C-n>
 
-      " <Leader>/ to clear search highlights
-      nnoremap <silent> <Leader>/ :nohls<CR>
-      "}}}
+        " <Leader>/ to clear search highlights
+        nnoremap <silent> <Leader>/ :nohls<CR>
+        "}}}
 
-      lua require("lsp")
+        lua require("lsp")
 
-      " Plugin configuration {{{
-      " LSP
-      set completeopt=menuone,noinsert,noselect
-      " }}}
+        " Plugin configuration {{{
+        " LSP
+        set completeopt=menuone,noinsert,noselect
+        " }}}
 
 
-      " Language specific commands {{{
-      augroup lang_viml
-        autocmd!
-        autocmd Filetype vim setlocal tabstop=2|setlocal shiftwidth=2| set expandtab
-      augroup END
+        " Language specific commands {{{
+        augroup lang_viml
+          autocmd!
+          autocmd Filetype vim setlocal tabstop=2|setlocal shiftwidth=2| set expandtab
+        augroup END
 
-      augroup lang_python
-        autocmd!
-        autocmd Filetype python setlocal tabstop=4|setlocal shiftwidth=4| setlocal expandtab
-        " allow expansion of triple quotes
-        autocmd Filetype python let b:delimitMate_nesting_quotes = ['"']
-      augroup END
+        augroup lang_python
+          autocmd!
+          autocmd Filetype python setlocal tabstop=4|setlocal shiftwidth=4| setlocal expandtab
+          " allow expansion of triple quotes
+          autocmd Filetype python let b:delimitMate_nesting_quotes = ['"']
+        augroup END
 
-      augroup lang_cpp
-        autocmd!
-        autocmd Filetype cpp setlocal tabstop=2|setlocal shiftwidth=2|setlocal expandtab
-      augroup END
+        augroup lang_cpp
+          autocmd!
+          autocmd Filetype cpp setlocal tabstop=2|setlocal shiftwidth=2|setlocal expandtab
+        augroup END
 
-      " }}}
+        " }}}
+      else
+        nnoremap ]b <Cmd>call VSCodeCall('workbench.action.nextEditorInGroup')<CR>
+        nnoremap [b <Cmd>call VSCodeCall('workbench.action.previousEditorInGroup')<CR>
+      endif
     '';
   };
 
--- vscode.nix
+++ vscode.nix
@@ -2,8 +2,16 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = [
-      pkgs.vscode-extensions.vscodevim.vim
-    ];
+    extensions = (with pkgs.vscode-extensions; [
+    ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+      name = "vscode-neovim";
+      publisher = "asvetliakov";
+      version = "0.0.82";
+      sha256 = "YUlygCPleF+/Ttyd2PeebAoZkcAhFmatbHi1nd6XwJ0=";
+    }];
+    userSettings = {
+      vscode-neovim.neovimExecutablePaths.darwin = "${pkgs.neovim-nightly}/bin/nvim";
+      vscode-neovim.neovimInitPath = "~/.config/nvim/init.vim";
+    };
   };
 }
