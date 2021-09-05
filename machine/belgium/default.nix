28d1eb869966347fc2bb72e887f936d0b512351d
Ole Kristian Pedersen <okpedersen@gmail.com>
2021-09-05 11:42:46 +0200

    defaults: Add macOS config

--- machine/belgium/default.nix
+++ machine/belgium/default.nix
@@ -4,6 +4,15 @@
     ../../common-dev-tools.nix
   ];
 
+  targets.darwin.defaults = {
+    "com.apple.dock" = {
+      # Changes here requires "killall Dock" to be run after switching
+      expose-group-apps = true;
+      autohide = true;
+      static-only = true;
+    };
+  };
+
   home.packages = with pkgs; [
     spotify
     # pandoc with pdf output
