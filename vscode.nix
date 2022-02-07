2efb29e7332480dbb4d90cb09f208e58db83d769
Ole Kristian Pedersen <okpedersen@gmail.com>
2022-02-07 20:52:33 +0100

    vscode: Update global settings

--- vscode.nix
+++ vscode.nix
@@ -10,9 +10,24 @@
       sha256 = "YUlygCPleF+/Ttyd2PeebAoZkcAhFmatbHi1nd6XwJ0=";
     }];
     userSettings = {
+      files.eol = "\n";
+      editor.insertSpaces = true;
+      editor.formatOnSave = true;
+      editor.formatOnPaste = true;
+
       vscode-neovim.neovimExecutablePaths.darwin = "${pkgs.neovim}/bin/nvim";
       vscode-neovim.neovimInitPath = "~/.config/nvim/init.vim";
+
+      "[csharp]" = {
+        "editor.tabSize" = 4;
+        "editor.defaultFormatter" = "ms-dotnettools.csharp";
+      };
+      csharp.semanticHighlighting.enabled = true;
       omnisharp.enableDecompilationSupport = true;
+      omnisharp.enableAsyncCompletion = true;
+      omnisharp.enableRoslynAnalyzers = true;
+      omnisharp.enableImportCompletion = true;
+      omnisharp.organizeImportsOnFormat = true;
     };
   };
 }
