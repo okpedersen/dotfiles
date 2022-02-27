6271ff1aaefffb1700e3924198a04db2193fc835
Ole Kristian Pedersen <okpedersen@gmail.com>
2022-02-27 15:43:12 +0100

    karabiner: Fix config files

--- karabiner/default.nix
+++ karabiner/default.nix
@@ -1,6 +1,4 @@
 { ... }:
 {
-  xdg.configFile."karabiner/karabiner.json".source = ./karabiner.json;
-  xdg.configFile."karabiner/assets/complex_modifications/karabiner_norwegian_with_caps_lock.json".source = ./karabiner_norwegian_with_caps_lock.json;
-  xdg.configFile."karabiner/assets/karabiner_switch_paragraph_sign_and_backquote.json".source = ./karabiner_switch_paragraph_sign_and_backquote.json;
+  xdg.configFile."karabiner".source = ./configFiles;
 }
