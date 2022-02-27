a0bf7dc501e51fbc32e0c413b46d126853b25eb3
Ole Kristian Pedersen <okpedersen@gmail.com>
2022-02-27 22:14:34 +0100

    nix flake: Refactor using flake-utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Home configuration";
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -10,7 +11,7 @@
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixpkgs-stable, nixpkgs-master, ... }:
+  outputs = inputs@{ self, flake-utils, nixpkgs, home-manager, nixpkgs-stable, nixpkgs-master, ... }:
     let
       nixpkgsOverlay = self: super: {
         stable = nixpkgs-stable.legacyPackages.${super.system};
@@ -31,42 +32,61 @@
       luaLanguageServerOverlay = import ./overlays/sumneko-lua-language-server.nix;
       omnisharpOverlay = import ./overlays/omnisharp-roslyn.nix;
       azureFunctionCoreToolsOverlay = import ./overlays/azure-functions-core-tools.nix;
+
+      overlays = [
+        (import ./spotify.nix)
+        nixpkgsOverlay
+        podmanOverlay
+        ipythonOverlay
+        luaLanguageServerOverlay
+        omnisharpOverlay
+        azureFunctionCoreToolsOverlay
+      ];
+
+      configurations = [
+        {
+          username = "ole.pedersen";
+          imports = [ ./machine/belgium ];
+          homeDirectory = "/Users/olekristianpedersen";
+        }
+        {
+          username = "ole.kristian.eidem.pedersen";
+          imports = [ ./machine/venezuela ];
+          homeDirectory = "/Users/ole.kristian.eidem.pedersen";
+        }
+        {
+          username = "docker";
+          imports = [ ./minimal.nix ./neovim.nix ];
+          homeDirectory = "/home/docker";
+        }
+      ];
+
     in
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+      (system: {
+        defaultPackage = home-manager.defaultPackage.${system};
+
+        packages.homeConfigurations = (builtins.listToAttrs
+          (map
+            (conf: {
+              name = "${conf.username}";
+              value = home-manager.lib.homeManagerConfiguration {
+                configuration = {
+                  nixpkgs.config.allowUnfree = true;
+                  nixpkgs.overlays = overlays;
+                  imports = conf.imports;
+                };
+                system = system;
+                username = conf.username;
+                homeDirectory = conf.homeDirectory;
+                stateVersion = "21.05";
+              };
+            })
+            configurations
+          )
+        );
+      }) //
     {
-
-      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-      defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
-
-      homeConfigurations."ole.pedersen" = home-manager.lib.homeManagerConfiguration {
-        configuration = {
-          nixpkgs.config.allowUnfree = true;
-          imports = [ ./machine/belgium { nixpkgs.overlays = [ (import ./spotify.nix) nixpkgsOverlay podmanOverlay ipythonOverlay luaLanguageServerOverlay omnisharpOverlay azureFunctionCoreToolsOverlay ]; } ];
-        };
-        system = "x86_64-darwin";
-        username = "ole.pedersen";
-        homeDirectory = "/Users/olekristianpedersen";
-        stateVersion = "21.05";
-      };
-
-      homeConfigurations."ole.kristian.eidem.pedersen" = home-manager.lib.homeManagerConfiguration {
-        configuration = {
-          nixpkgs.config.allowUnfree = true;
-          imports = [ ./machine/venezuela { nixpkgs.overlays = [ (import ./spotify.nix) nixpkgsOverlay podmanOverlay ipythonOverlay luaLanguageServerOverlay omnisharpOverlay azureFunctionCoreToolsOverlay ]; } ];
-        };
-        system = "x86_64-darwin";
-        username = "ole.kristian.eidem.pedersen";
-        homeDirectory = "/Users/ole.kristian.eidem.pedersen";
-        stateVersion = "21.05";
-      };
-
       belgium = self.homeConfigurations."ole.pedersen".activationPackage;
-
-      homeConfigurations.docker = home-manager.lib.homeManagerConfiguration {
-        configuration = { imports = [ ./minimal.nix ./neovim.nix { nixpkgs.overlays = [ ]; } ]; };
-        system = "x86_64-linux";
-        username = "docker";
-        homeDirectory = "/home/docker";
-        stateVersion = "21.05";
-      };
     };
 }
