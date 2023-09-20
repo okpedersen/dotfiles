af6aab0e286190ff7fb3cc6c8e4a3586f605a34f
Ole Kristian Pedersen <okpedersen@gmail.com>
2023-09-20 10:45:58 +0200

    shell: Add ulid utilities

--- machine/venezuela/default.nix
+++ machine/venezuela/default.nix
@@ -25,6 +25,12 @@ in
     DOTNET_ROOT = "${sdks}";
   };
 
+  home.shellAliases = {
+    bin2ulid="${pkgs.python3}/bin/python3 -c 'y = int(input().strip(),16); print(\"\".join(reversed([\"0123456789ABCDEFGHJKMNPQRSTVWXYZ\"[(y >> 5*i )& 31] for i in range(26)])))'";
+
+    ulid2bin="${pkgs.python3}/bin/python3 -c 'print(hex(sum(e << i*5 for i, e in enumerate(reversed([\"0123456789ABCDEFGHJKMNPQRSTVWXYZ\".index(c) for c in input().strip().upper()])))))'";
+  };
+
   home.packages = with pkgs; [
     go
     gopls
