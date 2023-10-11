e2ff21ae15f1dee87010e50b458375d26f5a59c4
Ole Kristian Pedersen <okpedersen@gmail.com>
2023-10-11 19:49:41 +0200

    nvim: Move Trouble conf + add file keybinds

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,3 +1,5 @@
+local wk = require("which-key")
+
 -- Map leaders
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
@@ -36,3 +38,23 @@ vim.api.nvim_create_autocmd("BufLeave", {
   group = ag_cursorline,
   callback = function() vim.opt.cursorline = false end
 })
+
+wk.register({
+  ['<leader>x'] = {
+    name = "+Trouble",
+    x = {'<cmd>TroubleToggle<CR>', "Toggle"},
+    w = {'<cmd>TroubleToggle workspace_diagnostics<CR>', "Workspace diagnostics"},
+    d = {'<cmd>TroubleToggle document_diagnostics<CR>', "Document diagnostics"},
+    q = {'<cmd>TroubleToggle quickfix<CR>', "Quickfix list"},
+    l = {'<cmd>TroubleToggle loclist<CR>', "Location list"},
+  },
+  ['[x'] = {function() require('trouble').previous({skip_groups = true, jump = true}) end, 'Trouble previous'},
+  [']x'] = {function() require('trouble').next({skip_groups = true, jump = true}) end, 'Trouble next'},
+
+  ['<leader>f'] = {
+    name = "+File",
+    f = {'<cmd>Telescope find_files<CR>', "Find file"},
+    g = {'<cmd>Telescope live_grep<CR>', "Live grep"},
+    s = {'<cmd>Telescope lsp_dynamic_workspace_symbols<CR>', "Workspace symbols"},
+  }
+},  { noremap = true, silent = true, mode = "n" })
